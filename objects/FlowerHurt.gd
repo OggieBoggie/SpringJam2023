5e4bdbc26d141735e64b8076eea09365c3005a9e
tommy <tommy.nguyen.vanc@gmail.com>
2023-04-29 09:41:31 -0700

    Added flower sprite template

---
+++ objects/FlowerHurt.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+@onready var collision_shape = $CollisionShape2D
---
+++ objects/FlowerTemplate.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+@onready var hurtbox = $FlowerHurt/CollisionShape2D
+@onready var animation = $AnimatedSprite2D
+
+var stats = PlayerStats
+
+func _on_flower_hurt_area_entered(area):
+	hurtbox.set_deferred("disabled", true)
+	animation.play("animate")
+	
